162523fee1d2c390245c29991a5277bab8936417
Kazunori Kajihiro <likerichie@gmail.com>
2014-09-10 07:37:06 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"io"
+	"log"
+	"net"
+	"os"
+	"strings"
+	"strconv"
+	"github.com/fsouza/go-dockerclient"
+)
+
+func getopt(name, def string) string {
+	if env := os.Getenv(name); env != "" {
+		return env
+	}
+	return def
+}
+
+func assert(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func proxyConn(conn net.Conn, addr string) {
+	backend, err := net.Dial("tcp", addr)
+	defer conn.Close()
+	if err != nil {
+		log.Println("proxy", err.Error())
+		return
+	}
+	defer backend.Close()
+
+	done := make(chan struct{})
+	go func() {
+		io.Copy(backend, conn)
+		backend.(*net.TCPConn).CloseWrite()
+		close(done)
+	}()
+	io.Copy(conn, backend)
+	conn.(*net.TCPConn).CloseWrite()
+	<-done
+}
+
+func findBackend(conn net.Conn, destPort int) string {
+	ip, _, _ := net.SplitHostPort(conn.RemoteAddr().String())
+	name,_ := inspectBackendName(ip, strconv.Itoa(destPort))
+	return lookupSRV(name)
+}
+
+func lookupSRV(name string) string {
+	_, addrs, err := net.LookupSRV("", "", name)
+	if err != nil {
+		log.Println("dns:", err)
+		return ""
+	}
+	if len(addrs) == 0 {
+		return ""
+	}
+	port := strconv.Itoa(int(addrs[0].Port))
+	return net.JoinHostPort(addrs[0].Target, port)
+}
+
+func inspectBackendName(sourceIP, destPort string) (string, error) {
+	envKey := "BACKEND_" + destPort
+
+	client, _ := docker.NewClient(getopt("DOCKER_HOST", "unix:///var/run/docker.sock"))
+
+	containers, err := client.ListContainers(docker.ListContainersOptions{})
+	if err != nil {
+		return "", errors.New("unable to list containers")
+	}
+	for _, listing := range containers {
+		container, err := client.InspectContainer(listing.ID)
+		if err != nil {
+			return "", errors.New("unable to inspect container " + listing.ID)
+		}
+		if container.NetworkSettings.IPAddress == sourceIP {
+			for _, env := range container.Config.Env {
+				parts := strings.SplitN(env, "=", 2)
+				if strings.ToLower(parts[0]) == strings.ToLower(envKey) {
+					return parts[1], nil
+				}
+			}
+			return "", errors.New("backend not found in container environment")
+		}
+	}
+	return "", errors.New("unable to find container with source IP")
+}
+
+func main() {
+	flag.Parse()
+	portRange := strings.Split(getopt("PORT_RANGE", "7000:7100"), ":")
+	pStart,_ := strconv.Atoi(portRange[0])
+	pEnd,_   := strconv.Atoi(portRange[1])
+	for i := pStart; i <= pEnd; i++ {
+		go func(port int) {
+			listener, err := net.Listen("tcp", ":"+strconv.Itoa(port))
+			assert(err)
+			for {
+				conn, err := listener.Accept()
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				backend := findBackend(conn, port)
+				if backend == "" {
+					conn.Close()
+					log.Printf("No backends! Closing the connection")
+					continue
+				}
+
+				go proxyConn(conn, backend)
+			}
+		}(i)
+	}
+}
